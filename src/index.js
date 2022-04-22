f48275ec2f5ab907da7ca5fed83de56e57fbd178
Amir Salehi <nik.amirsa@gmail.com>
2022-04-23 04:01:47 +0430

    change entry point of project by new inital in react 18

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
+</React.StrictMode>);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
