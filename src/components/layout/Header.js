a8a6cd71db6ede822d85562ecdb97aef67a03970
Amir Salehi <nik.amirsa@gmail.com>
2022-05-05 19:03:23 +0430

    set content to header component

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,10 @@
 export default function Header() {
     return (
         <header>
-
+            <h1>Posts by react and redux toolkit</h1>
+            <p>
+                simple project to use redux toolkit to fetch posts
+            </p>
         </header>
     )
 }
