98c70678ecef31e0264d1005edc47003ca2345c6
Amir Salehi <nik.amirsa@gmail.com>
2022-05-05 19:04:29 +0430

    load header to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './assets/styles/main.scss'
-import PostList from './features/post/PostList';
+import PostList from './features/post/PostList'
+import Header from './components/layout/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <PostList />
     </div>
   );
