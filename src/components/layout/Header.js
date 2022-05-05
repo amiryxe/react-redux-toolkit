89334bda6d461d9baa99c3602525da3338104963
Amir Salehi <nik.amirsa@gmail.com>
2022-05-05 19:06:21 +0430

    style: header content

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,8 @@
+import './Header.scss'
+
 export default function Header() {
     return (
-        <header>
+        <header className='header'>
             <h1>Posts by react and redux toolkit</h1>
             <p>
                 simple project to use redux toolkit to fetch posts
